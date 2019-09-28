89316918528a6d4ac5b9ab92ea8f1545ef2f0626
leonardo81 <leonardoeinstein2000@gmail.com>
2019-09-28 18:34:13 +0700

    clearing lanfing

--- src/components/carousel/Jurusan.js
+++ src/components/carousel/Jurusan.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ItemsCarousel from 'react-items-carousel';
 import styles from './Jurusan.module.css';
-import image from '../../images/cs.jpg';
 import image2 from '../../images/lumba-lumba.png';
 import image3 from '../../images/anemon.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -10,67 +9,60 @@ import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 export class Jurusan extends Component {
   state = {
-    children: [
-      <div style={{ maxWidth: '400px', maxHeight: '400px' }}>
-        <img
-          src={image}
-          style={{ maxWidth: '100%', maxHeight: '100%' }}
-          alt=''
-        />
-      </div>,
-      <div style={{ maxWidth: '400px', maxHeight: '400px' }}>
-        <img
-          src={image}
-          style={{ maxWidth: '100%', maxHeight: '100%' }}
-          alt=''
-        />
-      </div>
-    ],
     activeItemIndex: 0
   };
 
   changeActiveItem = activeItemIndex => this.setState({ activeItemIndex });
 
   render() {
-    const { children, activeItemIndex } = this.state;
+    const { activeItemIndex } = this.state;
+    const children = this.props.content;
 
     return (
-      <div
-        style={{
-          padding: '0 60px',
-          maxWidth: '400px',
-          maxHeight: '400px',
-          margin: '0 auto',
-          position: 'relative'
-        }}
-        className={styles.Jurusan}
-      >
-        <ItemsCarousel
-          numberOfCards={1}
-          gutter={12}
-          showSlither={false}
-          firstAndLastGutter={false}
-          freeScrolling={false}
-          // Active item configurations
-          requestToChangeActive={this.changeActiveItem}
-          activeItemIndex={activeItemIndex}
-          activePosition={'center'}
-          chevronWidth={88}
-          // rightChevron={<p style={{ zIndex: 99 }} className={styles.gede}>{'>'}</p>}
-          // leftChevron={<p style={{ zIndex: 99 }} className={styles.gede}>{'<'}</p>}
-          leftChevron = {<FontAwesomeIcon icon={faAngleLeft} className={styles.gede} />}
-          rightChevron = {<FontAwesomeIcon icon={faAngleRight} className={styles.gede} />}
-          outsideChevron={true}
-          className={styles.slidr}
-        >
-          {children}
-        </ItemsCarousel>
+      <div>
+      <div className={styles.box}>
+        <h2 className={styles.slideTitle} >{this.props.title}</h2>
         <div
-          className={
-            this.props.image === 1 ? styles.ImageWrapper : styles.ImageWrapper2
-          }
+          style={{
+            padding: '0 60px',
+            maxWidth: '350px',
+            maxHeight: '350px',
+            margin: '0 auto',
+            position: 'relative'
+          }}
+          className={styles.Jurusan}
         >
-          <img src={this.props.image === 1 ? image2 : image3} alt='' />
+          <ItemsCarousel
+            numberOfCards={1}
+            gutter={12}
+            showSlither={false}
+            firstAndLastGutter={false}
+            freeScrolling={false}
+            // Active item configurations
+            requestToChangeActive={this.changeActiveItem}
+            activeItemIndex={activeItemIndex}
+            activePosition={'center'}
+            chevronWidth={88}
+            // rightChevron={<p style={{ zIndex: 99 }} className={styles.gede}>{'>'}</p>}
+            // leftChevron={<p style={{ zIndex: 99 }} className={styles.gede}>{'<'}</p>}
+            leftChevron = {<FontAwesomeIcon icon={faAngleLeft} className={styles.gede} />}
+            rightChevron = {<FontAwesomeIcon icon={faAngleRight} className={styles.gede} />}
+            outsideChevron={true}
+            className={styles.slidr}
+          >
+            {children.map(c=>c.content)}
+          </ItemsCarousel>
+          <div
+            className={
+              this.props.image === 1 ? styles.ImageWrapper : styles.ImageWrapper2
+            }
+          >
+            <img src={this.props.image === 1 ? image2 : image3} alt='' />
+          </div>
+        </div>
+        </div>
+        <div className={styles.text}>
+          <p>{children[activeItemIndex].text}</p>
         </div>
       </div>
     );
