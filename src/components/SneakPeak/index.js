2c4846445a0415906ab681450744ba18354c90bc
Kefas Satrio Bangkit Solideantyo <kefassatrio@gmail.com>
2019-10-06 16:04:56 +0700

    masukin content

--- src/components/SneakPeak/index.js
+++ src/components/SneakPeak/index.js
@@ -3,7 +3,10 @@ import ItemsCarousel from 'react-items-carousel';
 import styles from './sneakpeak.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronCircleLeft, faChevronCircleRight } from '@fortawesome/free-solid-svg-icons'
- 
+import sneak1 from '../../images/pic1.jpg';
+import sneak2 from '../../images/pic2.jpg';
+import sneak3 from '../../images/pic3.jpg';
+import sneak4 from '../../images/pic4.jpg';
 
 export default class SneakPeak extends Component {
   state = {
@@ -14,22 +17,28 @@ export default class SneakPeak extends Component {
 
   render() {
     const { activeItemIndex } = this.state;
-    const children = [<div className={styles.anjay}/>,
-        <div className={styles.anjay}/>]
+    const children = [
+    <div className={styles.anjay}>
+      <img className={styles.anjayImg} src={sneak1} alt=""/>
+    </div>,
+    <div className={styles.anjay}>
+      <img className={styles.anjayImg} src={sneak2} alt=""/>
+    </div>,
+    <div className={styles.anjay}>
+      <img className={styles.anjayImg} src={sneak3} alt=""/>
+    </div>,
+    <div className={styles.anjay}>
+      <img className={styles.anjayImg} src={sneak4} alt=""/>
+    </div>
+  
+  ]
 
     return (
       <div>
         <h2 className={styles.title} >SneakPeak</h2>
         <div className={styles.box}>
             <div
-            style={{
-                padding: '0 60px',
-                maxWidth: '80vw',
-                maxHeight: 'auto',
-                margin: '0 auto',
-                position: 'relative'
-            }}
-            className={styles.Jurusan}
+            className={styles.sneakContainer}
             >
             <ItemsCarousel
                 numberOfCards={1}
